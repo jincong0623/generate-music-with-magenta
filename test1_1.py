fbcbb1c4e2301150a2810a78e540c68c2f153991
jincong0623 <jincong0623@126.com>
2017-08-05 15:15:47 +0800

    刘茜周报

---
+++ test1_1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  5 13:50:05 2017
+
+@author: lenovo
+"""
+
+str1=input("请输入一个人的名字")
+str2=input("请输入一个国家的名字")
+print("世界那么大，{}想去{}看看。".format(str1,str2))
